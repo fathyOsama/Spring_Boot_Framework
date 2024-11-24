f502b499dad0bdd81a4af20af56c4d33284a97b6
Fathy Osama <154971937+fathyOsama@users.noreply.github.com>
2024-11-24 15:58:49 +0300

    Revert "Revert "create CRUD Database Project""

---
+++ 06-spring-boot-spring-mvn/vaildationdemo/src/main/java/com/luv2code/springdemo/mvc/vaildationdemo/VaildationdemoApplication.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo.mvc.vaildationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaildationdemoApplication {
+
+	public static void main(String[] args) {SpringApplication.run(VaildationdemoApplication.class, args);}
+
+}
